16a7197159a622c1cfe0f4db8b7994fd26b4365f
dolors1966 <dolors.royo@upc.edu>
2025-07-01 15:38:53 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/prediccio", methods=["POST"])
+def prediccio():
+    dades = request.get_json()
+    # Aquí hi va el model
+    resultat = {"risc": "moderat"}  # simulació
+    return jsonify(resultat)
+
+if __name__ == "__main__":
+    app.run()
